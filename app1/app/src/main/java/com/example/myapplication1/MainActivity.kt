403f42a97df2cf464871e5251d9a812ed614fb0d
Maluu26 <maria.lfr.santos@edu.ufes.br>
2026-09-24 16:14:28 -0300

    Colocando botão e lógica do backend

--- app1/app/src/main/java/com/example/myapplication1/MainActivity.kt
+++ app1/app/src/main/java/com/example/myapplication1/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication1
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -22,6 +23,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener{
         binding.botaoImposto.setOnClickListener(this)
 
 
+
         /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -29,6 +31,19 @@ class MainActivity : AppCompatActivity(), View.OnClickListener{
         }*/
     }
         override fun onClick(view: View) {
-
+            if (view.id == R.id.botao_imposto){
+                val salario = binding.salario.text.toString().toFloatOrNull() ?: 0f
+                val gastos= binding.gastos.text.toString().toFloatOrNull() ?: 0f
+                val dependentes= binding.dependentes.text.toString().toFloatOrNull() ?: 0f
+                var  imposto = 0f
+                if (salario>5000){
+                    imposto = salario - gastos - (189.59F * dependentes)
+                    imposto = imposto * 0.275F
+                }
+                binding.inputImposto.text = "${imposto}"
+                Toast.makeText(applicationContext,
+                    "Imposto calculado com sucesso",
+                    Toast.LENGTH_SHORT).show()
+            }
         }
 }
